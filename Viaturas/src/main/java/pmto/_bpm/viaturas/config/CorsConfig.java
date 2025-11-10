5bd2e6ce8743888a7c68b4539f991a320bdc87af
Lucas Mota Reis <lucasmotareis@gmail.com>
2025-11-10 01:14:25 -0300

    fix(CORS): now CORS receives http localhost

--- Viaturas/src/main/java/pmto/_bpm/viaturas/config/CorsConfig.java
+++ Viaturas/src/main/java/pmto/_bpm/viaturas/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 public class CorsConfig {
@@ -13,9 +14,16 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Arrays.asList("*")); // Permite qualquer origem (ajuste depois)
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",        // FRONT LOCAL
+                "https://pmto8bpm.com.br",      // FRONT PRODUÇÃO (se tiver)
+                "https://www.pmto8bpm.com.br"   // WWW também
+        ));
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+
+        config.setExposedHeaders(List.of("Set-Cookie"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
