50c60c669fbf653534da84235099c6d8d62676db
Lucas Mota Reis <lucasmotareis@gmail.com>
2025-12-07 19:29:05 -0300

    fix(JWTToken):teste

--- Viaturas/src/main/java/pmto/_bpm/viaturas/auth/security/JwtAuthFilter.java
+++ Viaturas/src/main/java/pmto/_bpm/viaturas/auth/security/JwtAuthFilter.java
@@ -31,8 +31,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String path = request.getServletPath();
-        return path.startsWith("/auth/");
+        return path.equals("/auth/login") || path.equals("/auth/register");
     }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
